73e09bc9630cb55490aad7bf5f2c78f318e2c54b
Nicky Lalrochhara <nickylalrochhara917@gmail.com>
2023-06-14 17:25:05 +0530

    Update preinstall.sh

--- preinstall.sh
+++ preinstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ ! -z "$lalrochhara_ACCESS_TOKEN" ]]; then
-    echo "Downloading lalrochhara_kit..."
-    poetry run pip install git+https://${lalrochhara_ACCESS_TOKEN}@github.com/lalrochhara/lalrochhara_kit@v0.1.2
+if [[ ! -z "$USERBOTINDO_ACCESS_TOKEN" ]]; then
+    echo "Downloading userbotindo_kit..."
+    poetry run pip install git+https://${USERBOTINDO_ACCESS_TOKEN}@github.com/userbotindo/userbotindo_kit@v0.1.2
 fi
