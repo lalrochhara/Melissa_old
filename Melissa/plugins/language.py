b566155df239bd9f74c0d79f4b55ee300e062b51
Nicky Lalrochhara <nickylalrochhara917@gmail.com>
2023-06-30 02:33:28 +0530

    Update language.py

--- Melissa/plugins/language.py
+++ Melissa/plugins/language.py
@@ -34,7 +34,7 @@ from Melissa import command, filters, listener, plugin, util
 LANG_FLAG = {
     "en": f"{emoji.FLAG_UNITED_STATES} English",
     "id": f"{emoji.FLAG_INDONESIA} Indonesia",
-	"mz": f"{emoji.FLAG_INDIA} Mizoram",
+	"mz": f"{emoji.FLAG_INDIA} Mizo",
 }
 
 
